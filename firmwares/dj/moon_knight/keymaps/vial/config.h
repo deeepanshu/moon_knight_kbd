573696f373f5e19c9adff088c930551d63ef143c
Deepanshu Jain <deepanshu@Deepanshus-MacBook-Pro.local>
2025-11-26 21:51:05 +0700

    setup vial

---
+++ firmwares/dj/moon_knight/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0B, 0x7E, 0x88, 0x39, 0x38, 0xDE, 0xD3, 0x51}
+
+// Unlock key combination: Esc + Q (both on left half, top row)
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+// Allow live keymap editing
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
---
+++ firmwares/dj/moon_knight/keymaps/vial/rules.mk
@@ -0,0 +1,8 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
+
+# Disable features to save space
+MOUSEKEY_ENABLE = no
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
